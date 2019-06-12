f6bf9d953d84781fdbc0f5c5eff896a6b9c96511
Peter <pvroom@convergence-us.com>
2019-06-12 16:50:59 -0400

    v. 1.05

---
+++ node_modules/ionic2-alpha-scroll/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var module_1 = require("./module");
+exports.IonAlphaScrollModule = module_1.IonAlphaScrollModule;
+//# sourceMappingURL=index.js.map
